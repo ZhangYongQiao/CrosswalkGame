ebb960fc8b438a34a33bbef71581226ed92594ea
张永桥 <1348808364@qq.com>
2021-04-07 22:30:58 +0800

    重写代码提交

---
+++ CustomButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[AddComponentMenu("UI/CustomButton",200)]
+public class CustomButton : Button
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        base.OnPointerClick(eventData);
+        MessageCenter.Instance.Send(MessageName.OnPlaySoundEffect);
+    }
+}
---
+++ PrefabConst.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabConst
+{
+
+    #region UIPrefab
+    public const string MainPanel = "MainPanel";
+    public const string SettingPanel = "SettingPanel";
+
+
+    #endregion
+
+
+    #region OtherPrefab
+    public const string SoundEffects = "SoundEffect";
+    public const string SoundMusic = "SoundMusic";
+
+    #endregion
+
+
+}
